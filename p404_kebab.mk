5530b3214c68baf5c4d2131d3358bff91d13a24f
rounak1619 <rounakmehata9@gmail.com>
2021-08-04 12:54:39 +0000

    kebab:Welcome P404
    Signed-off-by: rounak1619 <rounakmehata9@gmail.com>

--- p404_kebab.mk
+++ p404_kebab.mk
@@ -16,24 +16,21 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from kebab device
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-PEX_BUILD_TYPE := OFFICIAL
-EXTRA_FOD_ANIMATIONS := true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    org.pex.build_maintainer=Rounak1619
+# Inherit some common P404 stuff.
+$(call inherit-product, vendor/404/configs/common.mk)
+$(call inherit-product, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/google/gms/config.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_kebab
+PRODUCT_NAME := p404_kebab
 PRODUCT_DEVICE := kebab
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
